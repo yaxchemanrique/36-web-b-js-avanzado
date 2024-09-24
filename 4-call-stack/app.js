aa53e525055d5fca05bbefc903e1ba3b6f494855
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-09-23 19:58:54 -0700

    feat: scope

---
+++ 4-call-stack/app.js
@@ -0,0 +1,46 @@
+/* 
+sayHi()
+
+function sayHi() {
+  console.log('hola!')
+} 
+*/
+
+/* const clickHandler = () => {
+  console.log('le diste click!')
+}
+
+element.addEventListener('click', clickHandler);
+ */
+/* sayHi()
+
+const sayHi = () => {
+  console.log('hola!');
+}
+ */
+
+/* console.log(x)
+var x = 3; */
+
+
+/* var x = 10;
+console.log(x) */
+
+let x = 10
+// console.log(x)
+
+function first() {
+  let x = 'hola'
+  let y = 'el valor de y'
+
+  if(3 > 2) {
+    let x = 'dentro del if'
+    let z = 'La ZEEETAAA!'
+    console.log(x)
+    console.log(y)
+  }
+
+  console.log(z)
+}
+
+first();
