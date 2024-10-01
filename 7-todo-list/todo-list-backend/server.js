a43ad2d7e1cfab1f50edbe461d71c72a964488f9
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-09-30 19:03:48 -0700

    feat: metodo get

--- 7-todo-list/todo-list-backend/server.js
+++ 7-todo-list/todo-list-backend/server.js
@@ -1,1 +1,34 @@
-console.log('Hola!')
+const express = require('express');
+const cors = require('cors');
+const DB_TASKS = require('./tasks.js');
+const PORT = 3000;
+
+// spread operator
+const tasks = [...DB_TASKS];
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/tasks', (req, res)=> {
+  res.send(tasks);
+})
+
+app.get('/tasks/:id', (req, res)=> {
+  // console.log(req.params)
+  const taskId = req.params.id;
+  // console.log(taskId)
+  let selectedTask = tasks.find((task) => task.id == taskId);
+  if(!selectedTask) {
+    res.statusCode = 404;
+    res.send(`El ID: ${taskId} no está en la base de datos`)
+  }
+
+  console.log(selectedTask)
+  res.send(selectedTask)
+})
+
+app.listen(PORT, () => {
+  console.log(`App escuchando en el puerto: ${PORT}`)
+})
+
---
+++ 7-todo-list/todo-list-backend/tasks.js
@@ -0,0 +1,19 @@
+let tasks = [
+  {
+    id: 1,
+    task: 'Sacar al perro',
+    status: 'done'
+  },
+  {
+    id: 2,
+    task: 'Estudiar ES06',
+    status: 'pending'
+  },
+  {
+    id: 3,
+    task: 'Empezar con proyecto final',
+    status: 'active'
+  },
+];
+
+module.exports = tasks;
